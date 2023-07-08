77b8e49627f8d6d3ff5928c7b5f49cfc33713d22
Natalia-Havashi <natali.gavashi@gmail.com>
2023-07-08 17:42:19 +0300

    Contact.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,21 +1,26 @@
+import { Contact } from "components/Contact/Contact";
 import PropTypes from 'prop-types';
-export const ContactList = ({filtered, deleteContact}) => {
+export const ContactList = ({ filtered, deleteContact }) => {
     return (
-        <ul>
-            {filtered.map(contact => (
-                <li key={contact.id}>{contact.name} : {contact.number}
-                <button
-                type="button"
-                onClick={()=>deleteContact(contact.id)}>
-                    Delete
-                </button>
-                </li>
-            ))}
-        </ul>
-    )
-}
-
-ContactList.propTypes = {
-    filtered: PropTypes.array.isRequired,
-    deleteContact: PropTypes.func.isRequired,
-}
+      <ul>
+        {filtered.map(contact => (
+          <Contact
+            key={contact.id}
+            contact={contact}
+            deleteContact={deleteContact}
+          />
+        ))}
+      </ul>
+    );
+  };
+  
+  ContactList.propTypes = {
+    filtered: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired
+      })
+    ).isRequired,
+    deleteContact: PropTypes.func.isRequired
+  };
